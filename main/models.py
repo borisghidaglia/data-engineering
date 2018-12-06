0ffe749be805ea94e514b90f8f9737a596b935a9
borisghidaglia <borisghidaglia@gmail.com>
2018-12-06 15:25:11 +0100

    POC of db querying through tripadvisor_crawler.utils.TripadvisorMongoDB() => no need to use models.py for now !

--- main/models.py
+++
@@ -1,6 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-# class Member(db.Model):
-#     pass
--- main/views.py
+++ main/views.py
@@ -2,10 +2,15 @@ from flask import render_template
 from . import app
 from os.path import abspath
 
+# The databases are based over there because I first developped tripadvisor_crawler,
+# and then I integrated it into this flask project.
+from tripadvisor_crawler.utils import TripadvisorMongoDB
+db = TripadvisorMongoDB()
+
 @app.route('/')
 def home():
-
-    return render_template('main/home.html')
+    users = db.make_query(collection_name='tripadvisor_review')
+    return render_template('main/home.html', users=users)
 
 @app.route('/login')
 def login():
