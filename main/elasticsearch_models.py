a00c74966877f1961c9ab24a0a69086d8375e019
borisghidaglia <borisghidaglia@gmail.com>
2018-12-14 18:36:58 +0100

    Elasticsearch in progress

---
+++ main/elasticsearch_models.py
@@ -0,0 +1,28 @@
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+
+from tripadvisor_crawler.utils import TripadvisorMongoDB
+mongo = TripadvisorMongoDB()
+
+class ElasticsearchDB():
+
+    def __init__(self, *args, **kwargs):
+        self.client = Elasticsearch()
+        bulk(self.client, self.bulk_index_mongo('tripadvisor_user'))
+        bulk(self.client, self.bulk_index_mongo('tripadvisor_review'))
+
+    def bulk_index_mongo(self, index):
+        data = list(mongo.db[index].find())
+        for elt in data:
+            doc_id = str(elt['_id'])
+            del elt['_id']
+            obj = {
+                "_index" : index,
+                "_type" : index+"_document",
+                "_id" : doc_id,
+                "_source" : elt
+            }
+            yield obj
+
+    def test(self):
+        return self.client.search(index="tripadvisor_user", body={"query": {"match_all": {}}})
--- main/views.py
+++ main/views.py
@@ -2,15 +2,18 @@ from flask import jsonify, render_template
 from . import app
 from os.path import abspath
 
-# The databases are based over there because I first developped tripadvisor_crawler,
+# The database Mongo is based over there because I first developped tripadvisor_crawler,
 # and then I integrated it into this flask project.
 from tripadvisor_crawler.utils import TripadvisorMongoDB
-db = TripadvisorMongoDB()
+from main.elasticsearch_models import ElasticsearchDB
+
+db_mongo = TripadvisorMongoDB()
+db_elastic = ElasticsearchDB()
 
 # Main routes
 @app.route('/')
 def home():
-    reviews = db.lazy_load('tripadvisor_review')
+    reviews = db_mongo.lazy_load('tripadvisor_review')
     col_1 = reviews[::3]
     col_2 = reviews[1::3]
     col_3 = reviews[2::3]
@@ -23,9 +26,10 @@ def home():
 
 @app.route('/search')
 def search():
-
+    data = db_elastic.test()
     return render_template(
         'main/search.html',
+        data = data
     )
 
 
@@ -33,5 +37,5 @@ def search():
 # API routes
 @app.route('/api/fetch-raw-reviews/<begin_at>')
 def fetch_raw_reviews(begin_at=0):
-    reviews = db.lazy_load('tripadvisor_review', begin_at=int(begin_at))
+    reviews = db_mongo.lazy_load('tripadvisor_review', begin_at=int(begin_at))
     return jsonify(reviews)
